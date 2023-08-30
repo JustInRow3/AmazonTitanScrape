8fb8a3df8ecb5c96f7927a21fed0a9c3c36a3866
Justine Rowe Jimenez <jimenezjustinerowe@gmail.com>
2023-08-30 13:43:45 +0800

    Open browser window

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-
+import time
 
 from selenium.webdriver.chrome.options import Options
 options = Options()
@@ -11,8 +11,8 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 # Open browser window
 wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-wd.implicitly_wait(10)
+wd.implicitly_wait(5)
 wd.get('https://www.amazon.com/')
 
-time.sleep(3)
+time.sleep(10)
 wd.close()
