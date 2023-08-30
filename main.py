59c46ceb4330c9a0b579b239e57c53ed3cd1c761
Justine Rowe Jimenez <jimenezjustinerowe@gmail.com>
2023-08-30 13:30:49 +0800

    Add modules and python packages.

---
+++ main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.page_load_strategy = 'none'
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+# Open browser window
+wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+wd.implicitly_wait(10)
+wd.get('https://www.amazon.com/')
+
+time.sleep(3)
+wd.close()
