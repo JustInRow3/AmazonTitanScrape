ffb861d161df72a996d964ec53e3f18de0b1feba
Justine Rowe Jimenez <jimenezjustinerowe@gmail.com>
2023-09-01 18:16:05 +0800

    Loop through table.

--- main.py
+++ main.py
@@ -14,9 +14,12 @@ options.add_extension('extension_8_3_0_0.crx')
 
 # Open browser window
 wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-wd.get('https://www.amazon.com/') # open amazon
 # bypass captcha by maximizing window
 wd.maximize_window()
+wd.get('https://www.amazon.com/') # open amazon
+print('Open tab')
+
+print('Maximize tab')
 wait = WebDriverWait(wd, 10) # setup wait
 original_window = wd.current_window_handle # store the ID of the original window
 
@@ -32,19 +35,37 @@ for tab in wd.window_handles:
 # id = "password"
 wd.find_element(By.ID, 'username').send_keys('karfafton@gmail.com')
 wd.find_element(By.ID, 'password').send_keys('@Dummy123')
+time.sleep(2)
 wd.find_element(By.CLASS_NAME, 'login').click()
 wd.close() # close tab
+print('Closed extension tab.')
 
 # Switch to main tab
 wd.switch_to.window(original_window)
 wd.refresh()
+print('Refresh page')
+time.sleep(5)
 
 # Input keywords
-#id="twotabsearchtextbox"
 keyword = 'wool dryer balls'
 wd.find_element(By.ID, 'twotabsearchtextbox').send_keys(keyword)
+print('Enter Keyword')
+wd.find_element(By.ID, 'nav-search-submit-button').click()
+print('Click submit')
+time.sleep(2)
 
-time.sleep(5)
+#select table
+#print(table.text.split('\n'))
+for i in range(100):
+    table = wd.find_element(By.ID, 'amazon-analysis-eefljgmhgaidffapnppcmmafobefjece')
+    print(i)
+    if 'Loading...' in table.text.split('\n'):
+        time.sleep(3)
+    else:
+        print(table.text)
+        break
+
+time.sleep(1)
 
 wd.close()
 wd.quit()
