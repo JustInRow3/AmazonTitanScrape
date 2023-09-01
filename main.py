60aebb46f465c77a095161400a56e0b0bc56e885
Justine Rowe Jimenez <jimenezjustinerowe@gmail.com>
2023-09-01 08:54:32 +0800

    Enabled the extension, seems good in 5 iterations

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import time
 
 from selenium.webdriver.chrome.options import Options
 options = Options()
-options.page_load_strategy = 'none'
+options.page_load_strategy = 'eager' #Webdriver waits until DOMContentLoaded event fire is returned.
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-#options.add_extension('./extension_8_3_0_0.crx')
+options.add_extension('extension_8_3_0_0.crx')
 
 # Open browser window
 wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 time.sleep(5)
 wd.get('https://www.amazon.com/')
-time.sleep(10)
+time.sleep(5)
+
 wd.close()
